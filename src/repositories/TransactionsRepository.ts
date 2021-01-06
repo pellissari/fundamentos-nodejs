fd26c285bad31d439b42e4c57ff33c66f865627d
pellissari <andredoamaral@yahoo.com.br>
2021-01-06 15:32:13 -0300

    income / outcome rules

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -4,6 +4,12 @@ interface Balance {
   income: number;
   outcome: number;
   total: number;
+};
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';  
 }
 
 class TransactionsRepository {
@@ -14,16 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+
+    return this.transactions;
+
   }
 
   public getBalance(): Balance {
     // TODO
-  }
 
-  public create(): Transaction {
-    // TODO
+    const {income, outcome} = this.transactions.reduce((balance, transaction) => {
+      transaction.type === 'income' ? balance.income += transaction.value : balance.outcome += transaction.value;
+      return balance;
+    },{
+      income:0,
+      outcome:0,
+    });
+
+    const total = income - outcome;
+
+    return {income, outcome, total};
+
+  } 
+
+  public create({title, value, type}: CreateTransactionDTO ): Transaction {
+    const transaction = new Transaction({title, value, type});
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
+
+
+
 
 export default TransactionsRepository;
